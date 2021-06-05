3e98bb3afd7d9b126101486a0c3b72fb4208a374
1onlinesolution <team@damianos.io>
2021-06-05 12:28:48 +0100

    export default options

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 exports.Logger = require('./lib/logger');
+exports.consoleOptions = require('./lib/defaultConsoleOptions');
+exports.fileOptions = require('./lib/defaultFileOptions');
+exports.mongoOptions = require('./lib/defaultMongoOptions');
