617178854d54d303f6024828bc398d7986d9f1a3
1onlinesolution <team@damianos.io>
2021-06-14 17:06:57 +0100

    improved formatting

--- test/logger.spec.js
+++ test/logger.spec.js
@@ -1,30 +1,68 @@
 const assert = require('assert');
 const Logger = require('../lib/logger');
+const consoleOptions = require('../lib/defaultConsoleOptions');
+const fileOptions = require('../lib/defaultFileOptions');
 
-const isProduction = process.env.NODE_ENV === 'production';
+consoleOptions.level = 'info';
 
 describe('Logger', () => {
   it('Default ctor', () => {
     const logger = new Logger({});
-    assert(logger.useConsole === !isProduction);
+    assert(logger.useConsole === false);
+    assert(logger.useFile === false);
+    assert(logger.useMongoDB === false);
+    assert(logger.transports !== null);
+    assert(logger.transports.length === 0);
+  });
+  it('Only file', () => {
+    const logger = new Logger({ useFile: true });
+    assert(logger.useConsole === false);
     assert(logger.useFile === true);
     assert(logger.useMongoDB === false);
     assert(logger.transports !== null);
-    assert(logger.transports.length === 2);
-    assert(logger.transports[0].name === 'console');
-    assert(logger.transports[1].name === 'file');
+    assert(logger.transports.length === 1);
+    assert(logger.transports[0].name === 'file');
   });
-  it('No console', () => {
-    const logger = new Logger({useFile: false});
-    assert(logger.useConsole === !isProduction);
+  it('Only console', () => {
+    const logger = new Logger({ useConsole: true });
+    assert(logger.useConsole === true);
     assert(logger.useFile === false);
     assert(logger.useMongoDB === false);
     assert(logger.transports !== null);
     assert(logger.transports.length === 1);
     assert(logger.transports[0].name === 'console');
   });
-  it('No file', () => {
-    const logger = new Logger({useConsole: false});
+});
+
+describe('Console', () => {
+  it('should print', () => {
+    const logger = new Logger({
+      label: 'my label',
+      level: 'info',
+      useConsole: true,
+      consoleOptions: consoleOptions,
+    });
+
+    logger.info('aaa', { item: 'bbb' });
+    assert(logger.useConsole === true);
+    assert(logger.useFile === false);
+    assert(logger.useMongoDB === false);
+    assert(logger.transports !== null);
+    assert(logger.transports.length === 1);
+    assert(logger.transports[0].name === 'console');
+  });
+});
+
+describe('File', () => {
+  it('should print', () => {
+    const logger = new Logger({
+      label: 'my label',
+      level: 'info',
+      useFile: true,
+      fileOptions: fileOptions,
+    });
+
+    logger.info('aaa', { item: 'bbb' });
     assert(logger.useConsole === false);
     assert(logger.useFile === true);
     assert(logger.useMongoDB === false);
